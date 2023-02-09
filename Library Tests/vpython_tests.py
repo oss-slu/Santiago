fe513c8fea62e4dbe1b93e05198137934b96d578
Greih Murray <murrayg0216@outlook.com>
2023-02-09 10:53:45 -0600

    Further tests with VPython
    Performed more tests with VPython. Now able to plot a cell properly but
    did require making changes to SBF-SEM Tools via MATLAB.
    
    Had to add a new function to StructureAPI.m file, function code below
    
    	function fv = allFV(obj)
                fv = obj.model.allFV;
    	end
    
    Added directly above render function already present in StructureAPI.m

--- Library Tests/vpython_tests.py
+++ Library Tests/vpython_tests.py
@@ -2,18 +2,30 @@ from vpython import *
 import random
 import matlab.engine
 from controllers.MLEngine import MLEngine
+from tqdm import tqdm
 
+# Cells
+# 127 - ~2500
+# 10912 - almost 11,000
+# 1321 - ~9000
 
+print('start')
 mle = MLEngine()
-
+print('gen neuron')
 neu = mle.createNeuron(127, 'i')
+print('add to path')
+mle.eng.addpath('C:/Users/murra/Documents/SLU/SLU_OSSC/MATLAB/SBFSEM-tools/+sbfsem/+render/')
+print('hitting cylinder')
+fv = mle.eng.render(neu);
+fv = mle.eng.allFV(neu)
 
-print('getting dict')
+verts = fv['vertices']
+faces = fv['faces']
 
-data = mle.nodesDictionary(neu)
-edge = mle.edgesList(neu, data)
-
-print(data)
+# print(type(fv))
+# print(fv.keys())
+# print(fv['vertices'])
+# print(len(fv['vertices']))
 
 scene.width = scene.height = 600
 
@@ -45,34 +57,44 @@ To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
 To pan left/right and up/down, Shift-drag.
 Touch screen: pinch/extend to zoom, swipe or two-finger rotate.""")
 
-color_list = [color.white, color.purple, color.blue, color.green, color.black, color.orange, color.cyan, color.red, color.yellow]
+i = 0
+for face in tqdm(faces, desc='Rendering'):
+    try:
+        a = vertex(pos=vec(verts[int(face[0]-1)][0], verts[int(face[0]-1)][1], verts[int(face[0]-1)][2]), color=color.red)
+        b = vertex(pos=vec(verts[int(face[1]-1)][0], verts[int(face[1]-1)][1], verts[int(face[1]-1)][2]), color=color.red)
+        c = vertex(pos=vec(verts[int(face[2]-1)][0], verts[int(face[2]-1)][1], verts[int(face[2]-1)][2]), color=color.red)
 
-all_points = []
+        t = triangle(vs=[a, b, c])
+    except IndexError:
+        print(faces[2])
 
-max_rad = 0
-
-for key in data.keys():
-    p = data.get(key)
-    print(p, key)
-    all_points.append({'pos': vec(p[0], p[1], p[2]), 'radius': 1, 'color': color_list[random.randint(0, len(color_list)-1)]})
-    if p[3] > max_rad:
-        max_rad = p[3]
-
-print(max_rad)
-
-c = points(pos=all_points, size_units='world')
-
-# half_range = int(sRange/2)
-# neg = -half_range
-
-# for i in range(0, N):
-#     v1 = random.randint(neg, half_range)
-#     v2 = random.randint(neg, half_range)
-#     v3 = random.randint(neg, half_range)
 #
-#     all_vs.append(vertex(pos=vec(v1, v2, v3), color=color_list[random.randint(0, len(color_list)-1)]))
+# color_list = [color.white, color.purple, color.blue, color.green, color.black, color.orange, color.cyan, color.red, color.yellow]
 #
-# for i in range(0, len(all_vs)-2):
-#     t = triangle(vs=all_vs[i:i+3])
-
-
+# all_points = []
+#
+# max_rad = 0
+#
+# for key in data.keys():
+#     p = data.get(key)
+#     all_points.append({'pos': vec(p[0], p[1], p[2]), 'radius': 1, 'color': color_list[random.randint(0, len(color_list)-1)]})
+#     if p[3] > max_rad:
+#         max_rad = p[3]
+#
+#
+# c = points(pos=all_points, size_units='world')
+#
+# # half_range = int(sRange/2)
+# # neg = -half_range
+#
+# # for i in range(0, N):
+# #     v1 = random.randint(neg, half_range)
+# #     v2 = random.randint(neg, half_range)
+# #     v3 = random.randint(neg, half_range)
+# #
+# #     all_vs.append(vertex(pos=vec(v1, v2, v3), color=color_list[random.randint(0, len(color_list)-1)]))
+# #
+# # for i in range(0, len(all_vs)-2):
+# #     t = triangle(vs=all_vs[i:i+3])
+#
+#
